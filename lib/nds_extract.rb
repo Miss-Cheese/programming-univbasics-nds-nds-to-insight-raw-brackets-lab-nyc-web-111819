10e075595f1f510b91182e6a2b712ac25226356b
Natalie Galligan <nat.galligan@gmail.com>
2019-11-10 02:28:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,8 +23,7 @@ def directors_totals(nds)
       total_movie_revenue += movies_array[new_counter][:worldwide_gross]
       new_counter += 1
     end
-
-    total_movie_revenue
+    new_hash[director_hash[:name]] = total_movie_revenue
 
     counter += 1
 
