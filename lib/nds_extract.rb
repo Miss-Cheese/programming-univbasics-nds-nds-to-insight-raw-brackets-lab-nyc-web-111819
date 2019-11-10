0da0654a53ff053c1263f4b1b2dbb304fb00798c
Natalie Galligan <nat.galligan@gmail.com>
2019-11-10 01:37:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,6 +3,7 @@ require 'directors_database'
 
 def directors_totals(nds)
 
+  pp nds
 
   counter = 0
 
@@ -11,10 +12,7 @@ def directors_totals(nds)
     counter += 1
   end
 
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
+  
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
